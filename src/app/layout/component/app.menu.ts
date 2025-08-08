441bee758a9091fbdc4967f213b02fc94294ee0e
badrsa10 <33689798+badrsa10@users.noreply.github.com>
2025-08-08 08:36:05 +0000

    08082025

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -28,7 +28,7 @@ export class AppMenu {
                 label: 'Modules',
                 items: [
                     { label: 'Clients', icon: 'pi pi-fw pi-user', routerLink: ['/modules/clients'] },
-                    { label: 'Vehicules', icon: 'pi pi-fw pi-car', routerLink: ['/'] },
+                    { label: 'Vehicules', icon: 'pi pi-fw pi-car', routerLink: ['/modules/vehicules'] },
                     { label: 'Commandes', icon: 'pi pi-fw pi-cart-arrow-down', routerLink: ['/'] },
                     { label: 'Fournisseurs', icon: 'pi pi-fw pi-shop', routerLink: ['/'] },
                     { label: 'Pieces', icon: 'pi pi-fw pi-cog', routerLink: ['/modules/pieces'] },
--- src/app/pages/modules/modules.routes.ts
+++ src/app/pages/modules/modules.routes.ts
@@ -3,11 +3,13 @@ import { ClientsComponent } from "./clients/clients.component";
 import { ProfilClientComponent } from './clients/profil-client/profil-client.component';
 import { ServicesComponent } from './services/services.component';
 import { PiecesComponent } from './pieces/pieces.component';
+import { VehiculesComponent } from './vehicules/vehicules.component';
 
 
 export default [
     { path: 'clients', data: { breadcrumb: 'Clients' }, component: ClientsComponent },
     { path: 'client-profile/:id', data: { breadcrumb: 'Profil' }, component: ProfilClientComponent },
+    { path: 'vehicules', data: { breadcrumb: 'Pieces' }, component: VehiculesComponent },
     { path: 'pieces', data: { breadcrumb: 'Pieces' }, component: PiecesComponent },
     { path: 'services', data: { breadcrumb: 'Services' }, component: ServicesComponent },
     { path: '**', redirectTo: '/notfound' }
